f49c9a09fb2d35908c7759964fa70f92334ba42c
Ren <raulnino24@gmail.com>
2020-02-12 13:55:12 -0500

    added card append

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,5 @@
-
-// Raul code
-//Client ID
-//vLPrgG6PXNrL_cpL4E8SjA
-//yelp API key
-//yFGA5vRwWDXaOq0f6KaNtexkN5JjC-Bnl1X5kcfX2WLDLfyMbbtwfD9XoTF8cC8KqaWLwuaJ4fnRUJCNDmtEfB6IyvwTJtRvLeHF_gpMms8ZIRycX52NLnUkFOY-XnYx
-
-var city="new york";
-
+//var city=$("#city").val();
+var city="miami";
 var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=pizza&location="+city;
 
 $.ajax({
@@ -19,61 +12,51 @@ $.ajax({
     console.log(response);
     for (var i = 0; i < response.businesses.length; i++) {
      
-     
-      var newtitle =$("<h2>");
-      newtitle.html(response.businesses[i].name);
-      var locAddress =$("<h4>");
-      locAddress.html(response.businesses[i].location.address1);
-      var telPhone =$("<p>");
-      telPhone.html(response.businesses[i].display_phone);
-     
-      $("body").append(newtitle);
-      $("body").append(locAddress);
-      $("body").append(telPhone);
-
+        var card=$("<div>").attr("class","cardDiv")
       
-     
-
+        
+              var newtitle =$("<h2>");
+              newtitle.html(response.businesses[i].name);
+              var locAddress =$("<h4>");
+              locAddress.html(response.businesses[i].location.address1);
+              var telPhone =$("<p>");
+              telPhone.html(response.businesses[i].display_phone);
+              var busPicture=$('<img id="dynamic">')
+              busPicture.attr('src', response.businesses[i].image_url);
+      card.append(newtitle,locAddress,telPhone,busPicture)
+      
+      $("body").append(card);
   };
   });
+
+
     /*
-      <div class="col mb-3">
-     <!--event1 -->       
-          <div class="card">
-            <img src="assets/images/events.jpeg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 id="firstSpot"class="card-title">Title</h5>
-              <p  class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <a href="#" class="btn btn-warning">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-
-        var card =$("<div>");
-      card.html("this is my div");
-      $("body").append(card);
-
-
-
-
-
-      $("#firstSpot").html('<p>Business location: '+response.businesses[1].name+'</p>');
-      console.log(response.businesses[1].name[1]);
-
-      
-    $("#secondSpot").html('<p>Business location: '+response.businesses[2].name+'</p>');
-      console.log(response.businesses[2].name[1]);
-
-
-
-    /*var businesses = response.businesses;
+      <div class="row">
+ <div class="col mb-3">
+<!--event1 -->       
+   <div class="card">
+     <img src="assets/images/events.jpeg" class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 id="firstSpot"class="card-title">Title</h5>
+       <p  class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+       <a href="#" class="btn btn-warning">Go somewhere</a>
+     </div>
+   </div>
+ </div>   
+  var businesses = response.businesses;
 
     for (var i = 0; i < 7; i++) {
       var businessName = businesses[i].name;
 
       $('body').append('<h1>Business location: '+businesses[i].location.display_address[0]+'</h1>')
       console.log(businesses[i].location.display_address[0]);
-    }*/
+    
+    // Raul code
+    //Client ID
+    //vLPrgG6PXNrL_cpL4E8SjA
+    //yelp API key
+    //yFGA5vRwWDXaOq0f6KaNtexkN5JjC-Bnl1X5kcfX2WLDLfyMbbtwfD9XoTF8cC8KqaWLwuaJ4fnRUJCNDmtEfB6IyvwTJtRvLeHF_gpMms8ZIRycX52NLnUkFOY-XnYx
 
-
+!<----------------------------------------------------------------------------------------------------->>>>>>
+  }*/
 //Jose code
