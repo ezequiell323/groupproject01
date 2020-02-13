008d1be2772d49817603241fc769a17ed4dd5b72
Eze <eze.largaespada@gmail.com>
2020-02-12 20:21:13 -0500

    update3

--- assets/script/main.js
+++ assets/script/main.js
@@ -22,35 +22,24 @@ $.ajax({
       $(".containerRow").append(" <div class='col-4 mb-3'> <div class='card'> <img height='300' width='auto' src='"+ image + "'class='card-img-top alt='...'> <div class='card-body'><h5 id='firstSpot'class='card-title'>" + name + "</h5><p  class='card-text'>" + address + " " + telephone + "</p><a href='#' class='btn btn-warning'>Go somewhere</a></div> </div></div>");
   };
   });
+//Eze code
+var x = document.getElementById("nearMe");
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+    function showPosition(position) {
+      x.innerHTML = "Latitude: " + position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude;
+   
+   
+   
+   
+   
+    }
 
 
-    /*
-      <div class="row">
- <div class="col mb-3">
-<!--event1 -->       
-   <div class="card">
-     <img src="assets/images/events.jpeg" class="card-img-top" alt="...">
-     <div class="card-body">
-       <h5 id="firstSpot"class="card-title">Title</h5>
-       <p  class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-       <a href="#" class="btn btn-warning">Go somewhere</a>
-     </div>
-   </div>
- </div>   
-  var businesses = response.businesses;
-
-    for (var i = 0; i < 7; i++) {
-      var businessName = businesses[i].name;
-
-      $('body').append('<h1>Business location: '+businesses[i].location.display_address[0]+'</h1>')
-      console.log(businesses[i].location.display_address[0]);
-    
-    // Raul code
-    //Client ID
-    //vLPrgG6PXNrL_cpL4E8SjA
-    //yelp API key
-    //yFGA5vRwWDXaOq0f6KaNtexkN5JjC-Bnl1X5kcfX2WLDLfyMbbtwfD9XoTF8cC8KqaWLwuaJ4fnRUJCNDmtEfB6IyvwTJtRvLeHF_gpMms8ZIRycX52NLnUkFOY-XnYx
-
-!<----------------------------------------------------------------------------------------------------->>>>>>
-  }*/
 //Jose code
